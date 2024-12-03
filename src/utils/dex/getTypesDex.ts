e78a92f835f7262e5b51e1f2d1ff09f26eb53e30
Cody Spratford <koreanpanda345@gmail.com>
2024-12-02 16:51:14 -0800

    Using a direct path to the classes

--- src/utils/dex/getTypesDex.ts
+++ src/utils/dex/getTypesDex.ts
@@ -1,7 +1,7 @@
 import { type GenerationNum } from '@smogon/calc';
-import type * as I from '@smogon/calc/data/interface';
+import type * as I from '../node_modules/@smogon/calc/src/data/interface';
 // import { Types } from '@smogon/calc/dist/data/types';
-import {toID, extend} from '@smogon/calc/util';
+import {toID, extend} from '../node_modules/@smogon/calc/src/util';
 
 export type TypeChart = {
 	[type in I.TypeName]?: {[type in I.TypeName]?: number};
